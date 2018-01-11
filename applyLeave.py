c4ba5ede80897303d94e759e68c03336fc2c492d
Jayden Chua <jayden.chua@modix.cn>
2018-01-11 10:09:54 +0800

    add dev mode, fix html not showing in gmails, refactor html rows

--- applyLeave.py
+++ applyLeave.py
@@ -3,12 +3,44 @@ import json
 import inquirer
 from openpyxl import Workbook
 from openpyxl.styles import Font, Alignment, PatternFill
+from openpyxl.styles.borders import Border, Side
 import datetime
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
+
+DEV_MODE = True
+
+
+def main():
+    data = load_config_files()
+    confirm_send = False
+    answers = {}
+    common_headers = [
+        'Name',
+        'Department Shortcut Dimension',
+        'Request Type',
+        'Project Shortcut',
+        'LOTT Type',
+        'Detail Type for LOTT',
+        'Description',
+        'Start Date',
+        'Start Time',
+        'To Date',
+        'To Time',
+        'Quantity (Days)'
+    ]
+
+    while not confirm_send:
+        questions = set_questions(data)
+        answers = ask_questions(questions)
+        confirm_answer = ask_confirm(answers, questions)
+        confirm_send = confirm_answer['confirm']
+
+    set_worksheet(common_headers, answers, data)
+    send_email(common_headers, answers, data)
 
 
 def load_config_files():
@@ -20,30 +52,36 @@ def load_config_files():
 
 def set_questions(data):
     # Data can be assigned directly to cells
+    defaults = data["forms"]["leave"]["default"]
     questions = [
         inquirer.Text(
             'name',
             message="What is your Name?",
+            default=defaults["name"]
         ),
         inquirer.List(
             'department',
             message="Which Department?",
             choices=['Product', 'Production', 'Sales&Marketing', 'Management', 'Delivery', 'Development'],
+            default=defaults["department"]
         ),
         inquirer.List(
             'request_type',
             message="Which Request Type?",
             choices=['Team', 'Project', 'Presales'],
+            default=defaults["request_type"]
         ),
         inquirer.List(
             'project_type',
             message="Which Project Shortcut?",
             choices=data["user"]["projects"],
+            default=defaults["request_type"]
         ),
         inquirer.List(
             'lott_type',
             message="Which LOTT Type?",
             choices=['Leave', 'OT', 'Travel'],
+            default=defaults["lott_type"]
         ),
         inquirer.List(
             'lott_detail_type',
@@ -62,26 +100,43 @@ def set_questions(data):
                 'Paternity Leave',
                 'Personal Leave'
             ],
+            default=defaults["lott_detail_type"]
         ),
         inquirer.Text(
             'description',
             message="What is the purpose of the leave?",
+            default=defaults["description"]
         ),
         inquirer.Text(
             'start_date',
-            message="What is the Start Date?",
+            message="What is the Start Date? [DD/MM/YYYY]",
         ),
-        inquirer.Text(
+        inquirer.List(
             'start_time',
             message="What is the Start Time?",
+            choices=[
+                '09.00 AM',
+                '12.00 PM'
+            ],
+            default=defaults["start_time"]
         ),
         inquirer.Text(
             'end_date',
-            message="What is the End Date?",
+            message="What is the End Date? [DD/MM/YYYY]",
         ),
-        inquirer.Text(
+        inquirer.List(
             'end_time',
             message="What is the End Time?",
+            choices=[
+                '12.00 PM',
+                '06.00 PM'
+            ],
+            default=defaults["end_time"]
+        ),
+        inquirer.Text(
+            'weekend_ph',
+            message="Number of Weekend/PH between Start and End Date?",
+            default='0'
         )
     ]
 
@@ -90,7 +145,13 @@ def set_questions(data):
 
 def ask_questions(questions):
     answers = inquirer.prompt(questions)
-    answers['quantity'] = str(compute_leave_days_applied(answers['start_date'], answers['end_date']))
+    answers['quantity'] = str(compute_leave_days_applied(
+        answers['start_date'],
+        answers['end_date'],
+        answers['start_time'],
+        answers['end_time'],
+        answers['weekend_ph'])
+    )
     return answers
 
 
@@ -110,7 +171,7 @@ def ask_confirm(answers, questions):
     question = [
         inquirer.Confirm(
             'confirm',
-            message="Is the information good to apply?",
+            message="Proceed with Application?",
             default=True
         )
     ]
@@ -118,30 +179,17 @@ def ask_confirm(answers, questions):
     return is_confirm
 
 
-def set_worksheet(answers, data):
+def set_worksheet(common_headers, answers, data):
+    print('Preparing Excel Spreadsheet...')
     wb = Workbook()
     ws = wb.active
     ws.merge_cells('A1:L1')
 
-    style_font_main_header = Font(name='Calibri (Body)', size='24', bold=True)
-    style_font_sub_header = Font(name='Calibri (Body)', size='11', bold=True)
-    style_fill_light_grey = PatternFill(fill_type='solid', start_color='FFB7B7B7', end_color='FFB7B7B7')
-    style_fill_dark_grey = PatternFill(fill_type='solid', start_color='FF808080', end_color='FF808080')
-    style_alignment_center = Alignment(horizontal='centerContinuous', vertical='bottom')
-
     ws['A1'] = data["forms"]["leave"]["title"]
     a1 = ws['A1']
-    a1.alignment = style_alignment_center
-    a1.fill = style_fill_light_grey
-    a1.font = style_font_main_header
 
     # Rows can also be appended
-    ws.append(['Name', 'Department Shortcut Dimension', 'Request Type', 'Project Shortcut', 'LOTT Type', 'Detail Type for LOTT', 'Description', 'Start Date', 'Start Time', 'To Date', 'To Time', 'Quantity (Days)'])
-
-    for i in range(1, 13):
-        ws.cell(row=2, column=i).fill = style_fill_light_grey
-        ws.cell(row=2, column=i).font = style_font_sub_header
-        ws.cell(row=2, column=i).alignment = style_alignment_center
+    ws.append(common_headers)
 
     ws.append([
         answers['name'],
@@ -155,89 +203,118 @@ def set_worksheet(answers, data):
         answers['start_time'],
         answers['end_date'],
         answers['end_time'],
-        answers['quantity'],
+        answers['quantity']
     ])
+
+    style_font_main_header = Font(name='Calibri (Body)', size='24', bold=True)
+    style_font_sub_header = Font(name='Calibri (Body)', size='11', bold=True, color='FFFFFFFF')
+    style_fill_light_grey = PatternFill(fill_type='solid', start_color='FFB7B7B7', end_color='FFB7B7B7')
+    style_fill_dark_grey = PatternFill(fill_type='solid', start_color='FF808080', end_color='FF808080')
+    style_fill_light_blue = PatternFill(fill_type='solid', start_color='FFDDEBF6', end_color='FFDDEBF6')
+    style_fill_dark_blue = PatternFill(fill_type='solid', start_color='FF5E9CD3', end_color='FF5E9CD3')
+    style_alignment_center = Alignment(horizontal='centerContinuous', vertical='bottom')
+    style_thin_border = Border(
+        left=Side(style='thin'),
+        right=Side(style='thin'),
+        top=Side(style='thin'),
+        bottom=Side(style='thin')
+    )
+
+    a1.alignment = style_alignment_center
+    a1.fill = style_fill_light_grey
+    a1.font = style_font_main_header
+    a1.border = style_thin_border
+
+    for i in range(1, 13):
+        ws.cell(row=2, column=i).fill = style_fill_light_grey
+        ws.cell(row=2, column=i).font = style_font_sub_header
+        ws.cell(row=2, column=i).alignment = style_alignment_center
+        ws.cell(row=2, column=i).border = style_thin_border
+        ws.cell(row=3, column=i).fill = style_fill_light_blue
+        ws.cell(row=3, column=i).alignment = style_alignment_center
+        ws.cell(row=3, column=i).border = style_thin_border
+
+    for i in range(8, 12):
+        ws.cell(row=2, column=i).fill = style_fill_dark_grey
+
+    ws.cell(row=2, column=7).fill = style_fill_dark_blue
+
+    for col in ws.columns:
+        max_length = 0
+        column = col[0].column # Get the column name
+        for cell in col:
+            try: # Necessary to avoid error on empty cells
+                if len(str(cell.value)) > max_length:
+                    max_length = len(cell.value)
+            except:
+                pass
+        adjusted_width = max_length # Make your adjustments here
+        ws.column_dimensions[column].width = adjusted_width
+
     # Save the file
     wb.save(data["user"]["doc_folder"] + 'sample.xlsx')
+    print('Excel Spreadsheet Prepared...')
 
 
-def send_email(answers, data):
-    fromaddr = data["user"]["email"]
-    toaddr = data["user"]["email"]
+def send_email(common_headers, answers, data):
+    print('Preparing Email...')
+
+    if not DEV_MODE:
+        from_addr = data["user"]["email"]
+        to_addr = data["forms"]["leave"]["emailTo"]
+        cc_addr = data["forms"]["leave"]["emailCc"]
+    else:
+        from_addr = data["dev"]["from_addr"]
+        to_addr = data["dev"]["to_addr"]
+        cc_addr = data["dev"]["cc_addr"]
 
     msg = MIMEMultipart()
 
-    msg['From'] = fromaddr
-    msg['To'] = toaddr
-    msg['Subject'] = 'Leave Application for ' + answers['name'] + 'from ' + answers['start_date']
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Cc'] = cc_addr
 
-    body = '<style>' \
-           '    .td-title {' \
-           '        padding: 8px; ' \
-           '        background: #ddd; ' \
-           '        font-weight: bold;' \
-           '    } ' \
-           '    .td-normal {' \
-           '        padding: 8px;' \
-           '    }' \
-           '</style>' \
-           '<table>' \
-           '    <tbody>'
+    msg['Subject'] = answers['lott_type'] + ' Application for ' + answers['name'] + ' [' + answers['start_date'] + ' - ' + answers['end_date'] + ']'
 
-    body += '        <tr>' \
-           '            <td class="td-title">Name</td>' \
-           '            <td class="td-normal">' + answers['name'] + '</td>' \
-           '        </tr>' \
-           '        <tr>' \
-           '            <td class="td-title">Department Shortcut Dimension</td>' \
-           '            <td class="td-normal">' + answers['department'] + '</td>' \
-           '        </tr>' \
-           '        <tr>' \
-           '            <td class="td-title">Request Type</td>' \
-           '            <td class="td-normal">' + answers['request_type'] + '</td>' \
-           '        </tr>' \
-           '        <tr>' \
-           '            <td class="td-title">Project Shortcut</td>' \
-           '            <td class="td-normal">' + answers['project_type'] + '</td>' \
-           '        </tr>' \
-           '        <tr>' \
-           '            <td class="td-title">LOTT Type</td>' \
-           '            <td class="td-normal">' + answers['lott_type'] + '</td>' \
-           '        </tr>' \
-           '        <tr>' \
-           '            <td class="td-title">Detail Type for LOTT</td>' \
-           '            <td class="td-normal">' + answers['lott_detail_type'] + '</td>' \
-           '        </tr>' \
-           '        <tr>' \
-           '            <td class="td-title">Description</td>' \
-           '            <td class="td-normal">' + answers['description'] + '</td>' \
-           '        </tr>' \
-           '        <tr>' \
-           '            <td class="td-title">Start Date</td>' \
-           '            <td class="td-normal">' + answers['start_date'] + '</td>' \
-           '        </tr>' \
-           '        <tr>' \
-           '            <td class="td-title">Start Time</td>' \
-           '            <td class="td-normal">' + answers['start_time'] + '</td>' \
-           '        </tr>' \
-           '        <tr>' \
-           '            <td class="td-title">To Date</td>' \
-           '            <td class="td-normal">' + answers['end_date'] + '</td>' \
-           '        </tr>' \
-           '        <tr>' \
-           '            <td class="td-title">To Time</td>' \
-           '            <td class="td-normal">' + answers['end_time'] + '</td>' \
-           '        </tr>' \
-           '        <tr>' \
-           '            <td class="td-title">Quantity (Days)</td>' \
-           '            <td class="td-normal">' + answers['quantity'] + '</td>' \
-           '        </tr>' \
-           '    </tbody>' \
-           '</table>'
+    common_answers = [
+        answers['name'],
+        answers['department'],
+        answers['request_type'],
+        answers['project_type'],
+        answers['lott_type'],
+        answers['lott_detail_type'],
+        answers['description'],
+        answers['start_date'],
+        answers['start_time'],
+        answers['end_date'],
+        answers['end_time'],
+        answers['quantity']
+    ]
+
+    style_title_cell = ' style="padding: 8px; background: #ddd; font-weight: bold;" '
+    style_normal_cell = ' style="padding: 8px;" '
+
+    body = ''
+    body += '<p>Hi Team, </p>'
+    body += '<p>I would like to apply for "' + answers['lott_type'] + '" for the period of ' + answers['start_date'] + ' - ' + answers['end_date'] + '.</p>'
+
+    body += '<table>'
+    body += '<tbody>'
+
+    for idx, header in enumerate(common_headers):
+        body += '<tr>'
+        body += '   <td ' + style_title_cell + '>' + header + '</td>'
+        body += '   <td ' + style_normal_cell + '>' + common_answers[idx] + '</td>'
+        body += '</tr>'
+
+    body += '</tbody>'
+    body += '</table>'
+
+    body += '<p>Regards<br/>' + data["user"]["name"] + '</p>'
 
     msg.attach(MIMEText(body, 'html'))
 
-    filename = "leave_application.xlsx"
+    filename = 'Attendance Request Form of ' + data["user"]["name"] + '.xlsx'
     attachment = open(data["user"]["doc_folder"] + "sample.xlsx", "rb")
 
     part = MIMEBase('application', 'octet-stream')
@@ -247,40 +324,41 @@ def send_email(answers, data):
 
     msg.attach(part)
 
+    print('Contacting SMTP Server...')
     server = smtplib.SMTP(data["smtp"]["server"], data["smtp"]["server_port"])
     server.starttls()
-    server.login(fromaddr, data["smtp"]["password"])
+
+    print('Logging into SMTP Server...')
+    server.login(from_addr, data["smtp"]["password"])
     text = msg.as_string()
-    server.sendmail(fromaddr, toaddr, text)
+
+    print('Sending Email...')
+    combine_to_addr = to_addr.split(',') + cc_addr.split(',')
+    server.sendmail(from_addr, combine_to_addr, text)
     server.quit()
 
+    print('Email Sent...')
 
-def compute_leave_days_applied(str_start_date, str_end_date):
+
+def compute_leave_days_applied(str_start_date, str_end_date, str_start_time, str_end_time, str_weekend_ph):
     start_date = get_date(str_start_date)
     end_date = get_date(str_end_date)
     leave_days = end_date - start_date
-    return leave_days.days
+    num_of_days = float(leave_days.days) + 1
+
+    if str_start_time == '12.00 PM':
+        num_of_days = num_of_days - 0.5
+    if str_end_time == '12.00 PM':
+        num_of_days = num_of_days - 0.5
+    if float(str_weekend_ph) != 0.0:
+        num_of_days = num_of_days - float(str_weekend_ph)
+    return num_of_days
 
 
 def get_date(str_date):
-    list_date = str_date.split('-')
+    list_date = str_date.split('/')
     date_proper = datetime.date(int(list_date[2]), int(list_date[1]), int(list_date[0]))
     return date_proper
-
-
-def main():
-    data = load_config_files()
-    confirm_send = False
-    answers = {}
-
-    while not confirm_send:
-        questions = set_questions(data)
-        answers = ask_questions(questions)
-        confirm_answer = ask_confirm(answers, questions)
-        confirm_send = confirm_answer['confirm']
-
-    set_worksheet(answers, data)
-    send_email(answers, data)
 
 
 main()
--- deployReport.py
+++ deployReport.py
@@ -6,6 +6,8 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+DEV_MODE = False
+
 with open('config.json') as json_data_file:
     config = json.load(json_data_file)
 
@@ -22,44 +24,46 @@ issue_results = r.json()
 if len(issue_results['issues']) > 0:
     now = datetime.now()
 
+    style_title_cell = ' style="padding: 8px; background: #ddd; font-weight: bold;" '
+    style_normal_cell = ' style="padding: 8px;" '
+
     body = '<p>Hi Team, </p>'
 
-    body += '<style>' \
-            '    .td-title {' \
-            '        padding: 8px; ' \
-            '        background: #ddd; ' \
-            '        font-weight: bold;' \
-            '    } ' \
-            '    .td-normal {' \
-            '        padding: 8px;' \
-            '    }' \
-            '</style>' \
-
     body += '<p>Here are the tasks that have been released for Week {}.</p>'.format(now.strftime('%W'))
-
     body += '<table>'
-    body += '<thead><tr><td class="td-title">Key</td><td class="td-title">Summary</td></tr></thead>'
+    body += '<thead>'
+    body += '<tr>'
+    body += '<td ' + style_title_cell + '>Key</td>'
+    body += '<td ' + style_title_cell + '>Summary</td>'
+    body += '</tr>'
+    body += '</thead>'
     body += '<tbody>'
 
     for issue in issue_results['issues']:
         issue_key = issue['key']
         issue_summary = issue['fields']['summary']
         body += '<tr>'
-        body += '<td class="td-normal"><a href="{0}{1}">{1}</a></td>'.format(jira_base_url, issue_key)
-        body += '<td class="td-normal"><a href="{0}{1}">{2}</a></td>'.format(jira_base_url, issue_key, issue_summary)
+        body += '<td' + style_normal_cell + '><a href="{0}{1}">{1}</a></td>'.format(jira_base_url, issue_key)
+        body += '<td' + style_normal_cell + '><a href="{0}{1}">{2}</a></td>'.format(jira_base_url, issue_key, issue_summary)
         body += '</tr>'
 
     body += '<tbody></table>'
-    body += '<p>Regards<br/>Jayden Chua</p>'
+    body += '<p>Regards<br/>' + config["user"]["name"] + '</p>'
 
-    fromaddr = config["user"]["email"]
-    toaddr = config["report"]["deployment"]["emailTo"]
+    if not DEV_MODE:
+        from_addr = config["user"]["email"]
+        to_addr = config["report"]["deployment"]["emailTo"]
+        cc_addr = config["report"]["deployment"]["emailCc"]
+    else:
+        from_addr = config["dev"]["from_addr"]
+        to_addr = config["dev"]["to_addr"]
+        cc_addr = config["dev"]['cc_addr']
 
     msg = MIMEMultipart()
 
-    msg['From'] = fromaddr
-    msg['To'] = toaddr
-    msg['Cc'] = config["report"]["deployment"]["emailCc"]
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Cc'] = cc_addr
 
     msg['Subject'] = 'Deployed Tasks for Week ' + now.strftime('%W [%d %b %Y]')
 
@@ -67,9 +71,10 @@ if len(issue_results['issues']) > 0:
 
     server = smtplib.SMTP(config["smtp"]["server"], config["smtp"]["server_port"])
     server.starttls()
-    server.login(fromaddr, config["smtp"]["password"])
+    server.login(from_addr, config["smtp"]["password"])
     text = msg.as_string()
-    server.sendmail(fromaddr, toaddr, text)
+    combine_to_addr = to_addr.split(',') + cc_addr.split(',')
+    server.sendmail(from_addr, combine_to_addr, text)
     server.quit()
     print('Email Sent!')
 else:
